6040c5b3f38440c9b3c1d713845fb77c07a03585
daniel amodu <danielolayinka97@gmail.com>
2023-02-06 19:09:26 +0100

    starting

---
+++ littlelemon/restaurant/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('menu/',views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+]
---
+++ littlelemon/restaurant/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MenuSerializer
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
